ce64fbd8d41eef2e739a01a7f4c201d96c094c34
Roman Emilian <roman.emilian@outlook.com>
2017-07-05 14:06:39 +0800

    feat(library): add json class

---
+++ SharpGrabber.Library/Metadata/JSON.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using System.IO;
+
+namespace SharpGrabber.Library
+{
+	public class Json<T>
+	{
+		public Location Location { get; private set; }
+
+		public Json(Location location)
+		{
+			Location = location;
+		}
+
+		public T Deserialise()
+		{
+			T deserialised;
+
+			using (StreamReader file = File.OpenText(Location.Value)) {
+				JsonSerializer serializer = new JsonSerializer();
+				deserialised = (T)serializer.Deserialize(file, typeof(T));
+			}
+
+			return deserialised;
+		}
+
+		public void Serialise(T serialisableObject)
+		{
+			using (StreamWriter writer = new StreamWriter(Location.Value)) {
+				writer.WriteLine(JsonConvert.SerializeObject(serialisableObject, Formatting.Indented));
+			}
+		}
+	}
+}
