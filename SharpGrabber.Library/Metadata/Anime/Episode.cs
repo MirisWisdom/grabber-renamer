3b74b2b4c6148b314c4afa0eb2860c9b009c2f93
Roman Emilian <roman.emilian@outlook.com>
2017-07-05 14:06:44 +0800

    feat(library): add episode class

---
+++ SharpGrabber.Library/Metadata/Anime/Episode.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace SharpGrabber.Library
+{
+	public class Episode
+	{
+		[JsonProperty(PropertyName = "original")]
+		public string OriginalName { get; set; }
+
+		[JsonProperty(PropertyName = "real")]
+		public string RealName { get; set; }
+
+		public Episode(string originalName, string realName)
+		{
+			OriginalName = originalName;
+			RealName = realName;
+		}
+	}
+}
