854e7515b815fcd5653db4a3b23309404d219883
Roman Emilian <roman.emilian@outlook.com>
2017-07-05 14:07:07 +0800

    feat(cli): add simple console program class

---
+++ SharpGrabber.CLI/Program.cs
@@ -0,0 +1,35 @@
+﻿using SharpGrabber.Library;
+using System;
+using System.IO;
+
+namespace SharpGrabber.CLI
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string metadataPath;
+			string episodesPath;
+
+			if (!File.Exists(args[0]) || !args[0].Contains(".json")) {
+				Console.WriteLine("Please select a valid JSON file!");
+				Environment.Exit(1);
+			}
+
+			if (!Directory.Exists(args[1])) {
+				Console.WriteLine("Please select a valid directory!");
+				Environment.Exit(1);
+			}
+
+			metadataPath = args[0];
+			episodesPath = args[1];
+
+			Json<Anime> json = new Json<Anime>(new Location(metadataPath));
+			Anime anime = json.Deserialise();
+			anime.RenameEpisodes(new Folder(new Location(episodesPath)));
+
+			Console.WriteLine("Finished!");
+			Console.ReadLine();
+		}
+	}
+}
