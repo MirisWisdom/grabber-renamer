3646cb179f09f0cebf627c415b6fb93fcd3d073a
Roman Emilian <roman.emilian@outlook.com>
2017-07-05 14:06:33 +0800

    feat(library): add folder class

---
+++ SharpGrabber.Library/Metadata/Folder.cs
@@ -0,0 +1,12 @@
+﻿namespace SharpGrabber.Library
+{
+	public class Folder
+	{
+		public Location Location { get; private set; }
+
+		public Folder(Location location)
+		{
+			Location = location;
+		}
+	}
+}
